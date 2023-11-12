da79d35cbcbc531519ddb15802cc978da28ad6af
Alexander Panov <panov@royalzsoftware.de>
2023-11-12 11:05:06 +0100

    fix: separate concerns for http client

--- src/http/http-repository.ts
+++ src/http/http-repository.ts
@@ -1,46 +1,42 @@
 import { CrudRepository } from "../crud-repository";
 import { Id } from "../id";
 import { PersistedModel } from "../model-base";
+import { CreateRepositoryOperation, DeleteRepositoryOperation, GetAllRepositoryOperation, GetDetailsRepositoryOperation, UpdateRepositoryOperation } from "../repository-operations";
 import { HttpClient } from "./http-client";
 
 export type RouteFnWithModelId<ModelType> = (id: Id<ModelType>) => string;
 
 export class JsonHttpApiClient implements HttpClient<object> {
-    
+
     constructor(private _httpClient: HttpClient<string>) { }
 
     send(url: string, method: "POST" | "PUT" | "PATCH" | "GET" | "DELETE", headers?: { [key: string]: string; } | undefined): Promise<{ status: number; body: object; }> {
-        return this._httpClient.send(url, method, {...headers, 'Content-type': 'application/json'}).then((response) => {
-            return {...response, body: JSON.parse(response.body)};
+        return this._httpClient.send(url, method, { ...headers, 'Content-type': 'application/json' }).then((response) => {
+            return { ...response, body: JSON.parse(response.body) };
         });
     }
 
     sendWithBody(url: string, method: "POST" | "PUT" | "PATCH", data: object, headers?: { [key: string]: string; } | undefined): Promise<{ status: number; body: object; }> {
-        return this._httpClient.sendWithBody(url, method, JSON.stringify(data), {...headers, 'Content-type': 'application/json'}).then((response) => {
-            return {...response, body: JSON.parse(response.body)};
+        return this._httpClient.sendWithBody(url, method, JSON.stringify(data), { ...headers, 'Content-type': 'application/json' }).then((response) => {
+            return { ...response, body: JSON.parse(response.body) };
         });
     }
 }
 
-export abstract class HttpRepository<ModelType, FilterType = {}> implements CrudRepository<ModelType, FilterType> {
-
-    protected abstract _routeDefinitions: {
-        create: string,
-        getAll: string,
-        getDetailsFor: RouteFnWithModelId<ModelType>,
-        update: RouteFnWithModelId<ModelType>,
-        delete: RouteFnWithModelId<ModelType>,
-    }
-
-    constructor(protected _httpClient: JsonHttpApiClient) { }
+export class HttpCreateRepositoryOperation<ModelType extends {}> implements CreateRepositoryOperation<ModelType> {
+    constructor(private _httpClient: JsonHttpApiClient, private _endpoint: string) { }
 
     create(model: ModelType): Promise<PersistedModel<ModelType>> {
-        return this._httpClient.sendWithBody(this._routeDefinitions.create, 'POST', model as object).then((result) => {
+        return this._httpClient.sendWithBody(this._endpoint, 'POST', model).then((result) => {
             return new PersistedModel<ModelType>((result.body as any).id, model)
         });
     }
+}
+
+export class HttpGetAllRepositoryOperation<ModelType extends {}, FilterType = {}> implements GetAllRepositoryOperation<ModelType, FilterType> {
+    constructor(private _httpClient: JsonHttpApiClient, private _endpoint: string) { }
     getAll(filter: Partial<FilterType>): Promise<PersistedModel<ModelType>[]> {
-        return this._httpClient.send(this._routeDefinitions.getAll, 'GET').then((result) => {
+        return this._httpClient.send(this._endpoint, 'GET').then((result) => {
             if (!Array.isArray(result.body)) throw new Error("No array returned");
 
             return result.body.map((item) => {
@@ -51,20 +47,74 @@ export abstract class HttpRepository<ModelType, FilterType = {}> implements Crud
             })
         });
     }
+}
+
+export class HttpGetDetailsForRepositoryOperation<ModelType extends {}> implements GetDetailsRepositoryOperation<ModelType> {
+    constructor(private _httpClient: JsonHttpApiClient, private _endpoint: RouteFnWithModelId<ModelType>) { }
     getDetailsFor(id: Id<ModelType>): Promise<PersistedModel<ModelType> | undefined> {
-        return this._httpClient.send(this._routeDefinitions.getDetailsFor(id), 'GET').then((result) => {
+        return this._httpClient.send(this._endpoint(id), 'GET').then((result) => {
             return result.body == undefined ? undefined : new PersistedModel<ModelType>((result.body as any)?.id, result.body as any);
         });
     }
-    update(id: Id<ModelType>, updatePayload: ModelType): Promise<PersistedModel<ModelType>> {
-        return this._httpClient.sendWithBody(this._routeDefinitions.update(id), 'PUT', updatePayload as any).then((result: any) => {
+}
+
+export class HttpUpdateRepositoryOperation<ModelType extends {}> implements UpdateRepositoryOperation<ModelType> {
+    constructor(private _httpClient: JsonHttpApiClient, private _endpoint: RouteFnWithModelId<ModelType>) { }
+    update(id: Id<ModelType>, updatedModel: ModelType): Promise<PersistedModel<ModelType>> {
+        return this._httpClient.sendWithBody(this._endpoint(id), 'PUT', updatedModel as any).then((result: any) => {
             return new PersistedModel<ModelType>((result.body as any)?.id, result.body as any);
         });
     }
+
+}
+
+export class HttpDeleteRepositoryOperation<ModelType extends {}> implements DeleteRepositoryOperation<ModelType> {
+    constructor(private _httpClient: JsonHttpApiClient, private _endpoint: RouteFnWithModelId<ModelType>) { }
     delete(id: Id<ModelType>): Promise<boolean> {
-        return this._httpClient.send(this._routeDefinitions.update(id), 'DELETE').then((result) => {
+        return this._httpClient.send(this._endpoint(id), 'DELETE').then((result) => {
             return result.status == 200;
         });
+    }
+
+}
+export abstract class HttpRepository<ModelType extends {}, FilterType = {}> implements CrudRepository<ModelType, FilterType> {
+
+    protected _createOperation: HttpCreateRepositoryOperation<ModelType>;
+    protected _getAllOperation: HttpGetAllRepositoryOperation<ModelType, FilterType>;
+    protected _getDetailsForOperation: HttpGetDetailsForRepositoryOperation<ModelType>;
+    protected _updateOperation: HttpUpdateRepositoryOperation<ModelType>;
+    protected _deleteOperation: HttpDeleteRepositoryOperation<ModelType>;
+
+    constructor(
+        protected _httpClient: JsonHttpApiClient,
+        protected _routeDefinitions: {
+            create: string,
+            getAll: string,
+            getDetailsFor: RouteFnWithModelId<ModelType>,
+            update: RouteFnWithModelId<ModelType>,
+            delete: RouteFnWithModelId<ModelType>,
+        }
+    ) {
+        this._createOperation = new HttpCreateRepositoryOperation<ModelType>(_httpClient, _routeDefinitions.create);
+        this._getAllOperation = new HttpGetAllRepositoryOperation<ModelType>(_httpClient, _routeDefinitions.getAll);
+        this._getDetailsForOperation = new HttpGetDetailsForRepositoryOperation<ModelType>(_httpClient, _routeDefinitions.getDetailsFor);
+        this._updateOperation = new HttpUpdateRepositoryOperation<ModelType>(_httpClient, _routeDefinitions.update);
+        this._deleteOperation = new HttpDeleteRepositoryOperation<ModelType>(_httpClient, _routeDefinitions.delete);
+    }
+    create(model: ModelType): Promise<PersistedModel<ModelType>> {
+        return this._createOperation.create(model);
+    }
+    getAll(filter: Partial<FilterType>): Promise<PersistedModel<ModelType>[]> {
+        return this._getAllOperation.getAll(filter);
+    }
+    getDetailsFor(id: Id<ModelType>): Promise<PersistedModel<ModelType> | undefined> {
+        return this._getDetailsForOperation.getDetailsFor(id);
+    }
+    update(id: Id<ModelType>, updatePayload: ModelType): Promise<PersistedModel<ModelType>> {
+        return this._updateOperation.update(id, updatePayload);
+    }
+    delete(id: Id<ModelType>): Promise<boolean> {
+        return this._deleteOperation.delete(id);
     }
 
 }
